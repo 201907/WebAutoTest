9006b05fb33041c518d44b2fda8d6ab68d070deb
左思誠 <zuo.sc@sunosoft.com.cn>
2020-01-20 16:42:28 +0900

    追加

--- WebAutoTest/WebAutoTest.py
+++ WebAutoTest/WebAutoTest.py
@@ -1,14 +1,17 @@
 import pandas as pd
-import io,sys,configparser
+import io,sys,configparser,os
 from selenium import webdriver
 
 config = configparser.ConfigParser()
+configFileLst = []
 url = ""
-
+configFolder = ""
 def init():
     global config
     global url
     global driver
+    global configFileLst
+    global configFolder
     config.read("mypy.ini")
     url = config.get("env","url")
     browser = config.get("env","browser")
@@ -21,8 +24,17 @@ def init():
     elif browser == "3":
         driver = webdriver.Opera()
     else:
-        print("没有找到对应的浏览器")
+        print("qi")
         sys.exit(0)
+    configFolder = config.get("env","actionFolder")
+    configFileLst = os.listdir(configFolder)
+
+def execute():
+    global configFolder
+    for configFile in configFileLst:
+        data = pd.read_excel(configFolder + "/" + configFile,sheet_name = "action")
+        print(data)
 
 if __name__ == "__main__":
     init()
+    execute()
