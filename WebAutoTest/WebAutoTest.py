75c5f648a022e2253460f97f95ed77aba020a8ef
左思誠 <zuo.sc@sunosoft.com.cn>
2020-01-22 18:50:51 +0900

    進捗更新

--- WebAutoTest/WebAutoTest.py
+++ WebAutoTest/WebAutoTest.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 import io,sys,configparser,os
 from selenium import webdriver
+from time import sleep
 
 config = configparser.ConfigParser()
 configFileLst = []
@@ -32,9 +34,26 @@ def init():
 def execute():
     global configFolder
     for configFile in configFileLst:
-        data = pd.read_excel(configFolder + "/" + configFile,sheet_name = "action")
-        print(data)
+        data = pd.read_excel(configFolder + "/" + configFile,sheet_name = "action",header = 0)
+        url = data.iloc[0,1]
+        driver.get(url)
+        action = data.iloc[2:,:]
+        for row in action.fillna(0).iterrows():
+            element = driver.find_element_by_css_selector(row[1].CSS)
+            action = row[1].action
+            time = row[1].time
+            input = row[1].input
+            time = row[1].time
+            if action == "输入内容":
+                element.clear()
+                element.send_keys(input)
+            elif action == "点击":
+                element.click()
+            elif action == "切换窗口":
+                driver.switch_to_window(input)
+
+            sleep(time)
 
 if __name__ == "__main__":
-    init()
-    execute()
+        init()
+        execute()
